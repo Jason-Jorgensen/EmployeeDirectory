39c7cdaa736577102534f6a446586e27300ba540
JJorgensen <jasoncjorgensen@gmail.com>
2021-04-25 15:58:26 -0600

    tailwinds installed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-
-
+// import React, { useState } from 'react';
+import React from 'react';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import EmployeeCard from './components/EmployeeCard';
 
 
 function App() {
+    // const [ searchTerm ]
+
+
+
     return(
         <div>
             <Header />
             <Wrapper>
                 <EmployeeCard />
-            </Wrapper>
+            </ Wrapper>
             <Footer />
         </div>
     )
 
 }
+
+export default App;
---
+++ src/components/EmployeeCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function EmployeeCard() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default EmployeeCard;
